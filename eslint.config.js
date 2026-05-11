c9292c0a179d1e66e945d970658d950498bd36cc
lsy-del <3378678076@qq.com>
2026-05-11 15:52:49 +0800

    项目初始化

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import pluginOxlint from 'eslint-plugin-oxlint'
+import pluginPrettier from 'eslint-plugin-prettier' // 必须加这行
+import skipFormatting from 'eslint-config-prettier/flat'
+
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{vue,js,mjs,jsx}'],
+    plugins: {
+      prettier: pluginPrettier // 必须加这行！声明 Prettier 插件
+    },
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true,
+          semi: false,
+          printWidth: 80,
+          trailingComma: 'none',
+          endOfLine: 'auto'
+        }
+      ],
+      'vue/multi-word-component-names': [
+        'warn',
+        {
+          ignores: ['index']
+        }
+      ],
+      'vue/no-setup-props-destructure': ['off'],
+      'no-undef': 'error'
+    }
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
+    }
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+
+  ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
+
+  skipFormatting
+])
