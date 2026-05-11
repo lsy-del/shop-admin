3ce432c640cb5b43380801dc591a20e14c47f711
lsy-del <3378678076@qq.com>
2026-05-11 19:27:50 +0800

    引入windicss工具

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import 'virtual:windi.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    }),
+    WindiCSS()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
