3ab7929b0a72e24f05985bf0032dcbc18331f91e
lsy-del <3378678076@qq.com>
2026-05-13 15:01:01 +0800

    @apply样式抽离

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -10,29 +10,25 @@ const onSubmit = () => {
 </script>
 
 <template>
-  <el-row class="min-h-screen bg-indigo-500">
+  <el-row class="login-container">
     <!-- 左边 -->
     <el-col :lg="16" :md="12">
-      <div class="h-full flex items-center justify-center">
+      <div class="left">
         <div>
-          <div class="font-bold text-5xl text-light-50 mb-4">欢迎光临</div>
-          <div class="text-gray-200 text-sm">
-            此站点是《vue3 + vite实战商城后台开发》视频课程的演示地址
-          </div>
+          <div>欢迎光临</div>
+          <div>此站点是《vue3 + vite实战商城后台开发》视频课程的演示地址</div>
         </div>
       </div>
     </el-col>
 
     <!-- 右边 -->
-    <el-col :lg="8" :md="12" class="bg-light-50">
-      <div class="h-full flex items-center justify-center flex-col">
-        <h2 class="font-bold text-3xl text-gray-800">欢迎回来</h2>
-        <div
-          class="flex items-center justify-center my-5 text-gray-300 space-x-2"
-        >
-          <span class="h-[1px] w-16 bg-gray-200"></span>
+    <el-col :lg="8" :md="12">
+      <div class="right">
+        <h2>欢迎回来</h2>
+        <div>
+          <span class="hr"></span>
           <span>账号密码登录</span>
-          <span class="h-[1px] w-16 bg-gray-200"></span>
+          <span class="hr"></span>
         </div>
 
         <el-form :model="form" class="w-[250px]">
@@ -65,3 +61,31 @@ const onSubmit = () => {
     </el-col>
   </el-row>
 </template>
+
+<style>
+.login-container {
+  @apply min-h-screen bg-indigo-500;
+}
+.login-container .left,
+.login-container .right {
+  @apply h-full flex items-center justify-center;
+}
+.login-container .right {
+  @apply flex-col bg-light-50;
+}
+.left > div > div:first-child {
+  @apply font-bold text-5xl text-light-50 mb-4;
+}
+.left > div > div:last-child {
+  @apply text-gray-200 text-sm;
+}
+.right > h2 {
+  @apply font-bold text-3xl text-gray-800;
+}
+.right > div {
+  @apply flex items-center justify-center my-5 text-gray-300 space-x-2;
+}
+.right .hr {
+  @apply h-[1px] w-16 bg-gray-200;
+}
+</style>
